0ee01cd01cd6b929cdbbe94114c4db2b5edb876f
Yuta Saito <kateinoigakukun@gmail.com>
2019-09-28 17:08:29 +0900

    Extract test case

--- Tests/SwiftTypeInferenceTests/TypeCheckerTests.swift
+++ Tests/SwiftTypeInferenceTests/TypeCheckerTests.swift
@@ -167,17 +167,30 @@ f(3)
     
     func testAssignConv() throws {
         let code = """
-let a: Int?? = 3
+let a: Int? = 3
 """
         let s = try Parser(source: code).parse()
         let tc = TypeChecker(source: s)
         try tc.typeCheck()
         
         let vd = try XCTCast(XCTArrayGet(s.statements, 0), VariableDecl.self)
+        _ = try XCTCast(XCTUnwrap(vd.initializer), InjectIntoOptionalExpr.self)
+    }
+
+    func testAssignOptionalOptionalConv() throws {
+        let code = """
+    let a: Int?? = 3
+    """
+        let s = try Parser(source: code).parse()
+        let tc = TypeChecker(source: s)
+        try tc.typeCheck()
+
+        let vd = try XCTCast(XCTArrayGet(s.statements, 0), VariableDecl.self)
 
         let initializer = try XCTCast(XCTUnwrap(vd.initializer), InjectIntoOptionalExpr.self)
         _ = try XCTCast(initializer.subExpr, InjectIntoOptionalExpr.self)
     }
+
     
     func testArgConvAssignConvInfer() throws {
         let code = """
