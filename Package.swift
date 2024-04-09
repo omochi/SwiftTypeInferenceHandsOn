e55d70354e03d4f887208079309ef74ac2048c53
S-Shimotori <gingerale.jagajaga@gmail.com>
2024-04-09 23:31:02 +0900

    update Package.swift to support Swift5.10

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.10
 
 import PackageDescription
 
@@ -7,8 +7,7 @@ let rpath = "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.
 let package = Package(
     name: "SwiftTypeInference",
     dependencies: [
-        .package(url: "https://github.com/apple/swift-syntax.git",
-                 .revision("swift-DEVELOPMENT-SNAPSHOT-2019-07-10-m")),
+        .package(url: "https://github.com/apple/swift-syntax.git", exact: "510.0.1"),
     ],
     targets: [
         .target(
@@ -21,7 +20,11 @@ let package = Package(
         ),
         .target(
             name: "SwiftcAST",
-            dependencies: ["SwiftSyntax", "SwiftcBasic", "SwiftcType"]
+            dependencies: [
+                .product(name: "SwiftSyntax", package: "swift-syntax"),
+                "SwiftcBasic",
+                "SwiftcType"
+            ]
         ),
         .target(
             name: "SwiftcSema",
@@ -35,7 +38,7 @@ let package = Package(
             name: "SwiftCompiler",
             dependencies: ["SwiftcAST", "SwiftcSema"]
         ),
-        .target(
+        .executableTarget(
             name: "swsc",
             dependencies: ["SwiftcAST", "SwiftcSema"],
             linkerSettings: [
